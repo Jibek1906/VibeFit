cbe3fd8b536dfa7da32bd434139d8d0191c673d4
Jibek1906 <jibek.cabirova@mail.ru>
2024-12-09 00:02:40 +0600

    user details page

--- app.py
+++ app.py
@@ -6,5 +6,13 @@ app = Flask(__name__)
 def main():
     return render_template('main.html')
 
+@app.route('/register')
+def register():
+    return render_template('registration.html')
+
+@app.route('/user-details')
+def user_details():
+    return render_template('user_details.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
